05889543309306d006573bf0f88946bb61b12b2a
satyavarshitha <155371756+satyavarshitha@users.noreply.github.com>
2024-02-25 21:40:25 +0530

    #include<stdio.h> int main() {     int n,i,sum=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++){         scanf("%d",&arr[i]);     }     for(i=1;i<n;i+=2){         sum+=arr[i];     }     printf("%d ",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=1;i<n;i+=2){
+        sum+=arr[i];
+    }
+    printf("%d
+",sum);
+}
